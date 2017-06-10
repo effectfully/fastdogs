7a45d3180611485057d3f7f9699f08590233f7a2
Sergey Mironov <grrwlf@gmail.com>
2017-06-10 15:48:28 +0300

    Version 0.5.0

---
+++ default.nix
@@ -0,0 +1,33 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, bytestring, containers, directory
+      , filepath, optparse-applicative, process, stdenv, text
+      }:
+      mkDerivation {
+        pname = "haskdogs";
+        version = "0.4.5";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          base bytestring containers directory filepath optparse-applicative
+          process text
+        ];
+        homepage = "http://github.com/grwlf/haskdogs";
+        description = "Generate tags file for Haskell project and its nearest deps";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
